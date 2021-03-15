23f4806f7c46a21cc402130eb9ca5ccc72cb1006
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-03-15 00:06:49 -0400

    Fibonacci calculator, calculate the nth value of the fibonacci sequence

---
+++ fibo.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#simple fibonacci nth digit calculator
+
+
+#define function
+def fibo(n):
+    
+    if n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
+    else:
+        return fibo(n-1) + fibo(n-2)
+
+
+
+print(fibo(10))
