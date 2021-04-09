0fe941c10c13421b8e0ea44c288056685ba57b76
akinwale <4412196+dewhallez@users.noreply.github.com>
2021-04-08 22:39:38 -0400

    update fibo.py to print list of nth digit

--- fibo.py
+++ fibo.py
@@ -13,6 +13,39 @@ def fibo(n):
     else:
         return fibo(n-1) + fibo(n-2)
 
-
-
 print(fibo(15))
+
+
+
+# To print a list of sequence to a given number
+for i in range(20):
+    print(i, fibo(i))
+
+
+
+# Sample output
+""" 
+    ➜  python_Projects git:(main) ✗ python3 fibo.py
+    610
+    0 0
+    1 1
+    2 1
+    3 2
+    4 3
+    5 5
+    6 8
+    7 13
+    8 21
+    9 34
+    10 55
+    11 89
+    12 144
+    13 233
+    14 377
+    15 610
+    16 987
+    17 1597
+    18 2584
+    19 4181
+
+"""
